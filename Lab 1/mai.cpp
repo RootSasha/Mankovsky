626c33c2479697fd8ad66371c376f27543dbc683
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2026-03-05 18:45:21 +0200

    Add main.cpp for calculating average of two numbers

---
+++ Lab 1/mai.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x = 1.45;
+    double z = 1.64;
+    double y;
+
+    y = (x + z) / 2; 
+
+    cout << "Variant 6" << endl;
+    cout << "x = " << x << ", z = " << z << endl;
+    cout << "Result y = " << y << endl;
+
+    return 0;
+}
