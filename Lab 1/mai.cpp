cea82054859d3a75bbe7ccd03815eff4bea22967
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2026-03-06 12:02:18 +0200

    Remove comment from calculation in mai.cpp
    Removed comment about calculation in a single variable.

--- Lab 1/mai.cpp
+++ Lab 1/mai.cpp
@@ -7,7 +7,6 @@ int main() {
     double y = 1.45, x = 1.64;
     double d = y - x;
 
-    // Обчислення прямо в одній змінній
     double b = 1 + abs(d) + (d * d / 2) + (d * d * d / 3);
 
     cout << "b = " << b << endl;
